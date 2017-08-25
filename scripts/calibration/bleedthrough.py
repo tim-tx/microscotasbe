b7305ed59fa89ab81b57cd0d6f32dc51aefdc2c6
Tim Jones <tjones01@gmail.com>
2017-08-25 11:20:35 -0400

    remove useless glob

--- scripts/calibration/bleedthrough.py
+++ scripts/calibration/bleedthrough.py
@@ -11,13 +11,12 @@ import logging
 import skimage.io as skio
 import numpy as np
 import matplotlib.pyplot as plt
-from glob import glob
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 path = "/mnt/seaside/rsl/DataSetForTim/11042016synNotch/CitationData/161104_083217_synNotch/161104_085013_Plate 2/"
 
-controls = {'r':'C4','g':'B4','b':'D4'}
+controls = {'r':'C4','g':'B4','b':'D4'} # these are well labels
 
 # build dictionary so we can ask for bleedthrough from red to green like mydict['r']['g']
 d = dict.fromkeys(['r','g','b'])
@@ -31,9 +30,9 @@ for c in controls.keys():
     temp_intensities = {'r':[],'g':[],'b':[]}
     for j in range(4):          # stitch number
         logging.info('stitch number %d' % (j+1))
-        r = skio.imread(glob(path + controls[c] + '_03_3_%d_Texas Red_%03d.tif' % (j+1,1))[0])
-        g = skio.imread(glob(path + controls[c] + '_03_2_%d_GFP_%03d.tif' % (j+1,1))[0])
-        b = skio.imread(glob(path + controls[c] + '_03_4_%d_TagBFP_%03d.tif' % (j+1,1))[0])
+        r = skio.imread(path + controls[c] + '_03_3_%d_Texas Red_%03d.tif' % (j+1,1))
+        g = skio.imread(path + controls[c] + '_03_2_%d_GFP_%03d.tif' % (j+1,1))
+        b = skio.imread(path + controls[c] + '_03_4_%d_TagBFP_%03d.tif' % (j+1,1))
         images = {'r':r,'g':g,'b':b}
 
         seg = mtcell.segment(images[c])
