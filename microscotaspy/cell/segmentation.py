7198519055f151f51895a3761dec8f9dfd22404d
Tim Jones <tjones01@gmail.com>
2018-04-18 10:43:32 -0400

    Improve speed of intensity calculation.

--- microscotaspy/cell/segmentation.py
+++ microscotaspy/cell/segmentation.py
@@ -5,11 +5,23 @@ import skimage.filters as skfi
 import skimage.measure as skmeas
 import skimage.morphology as skmorph
 import skimage.feature as skfeat
+import logging
+import time
+import numexpr as ne
+
+# if __debug__:
+#     import skimage.io as skio
+#     import datetime
+#     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+# else:
+#     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 def segment(im):
     smooth = skfi.gaussian(im)
     t = skfi.threshold_otsu(smooth)
     smooth_thresh = smooth > t
+    # if __debug__:
+    #     skio.imsave(datetime.datetime.now().isoformat() + '.png',smooth_thresh.astype(np.float))
     return smooth_thresh
 
 def declump(im,mask):
@@ -20,19 +32,26 @@ def declump(im,mask):
     labels_ws = skmorph.watershed(-distance, markers, mask=mask)
     return labels_ws
 
+def _intensity(im,mask,level):
+    cell_mask = np.where(mask == level)
+    return np.mean(im[cell_mask])
+
 def intensity(im,mask):
     levels = np.unique(mask)[1:]
     intensities = []
     for l in levels:
-        cell_mask = np.where(mask == l)
-        intensities.append(np.mean(im[np.where(mask == l)]))
+        intensities.append(np.mean(im[mask == l]))
     return np.array(intensities)
 
 def get_events(im,layers=(),labels=('default',)):
     assert(len(labels) == len(layers)+1)
+
     seg = segment(im)
     features = declump(im,seg)
-    df = pd.DataFrame({labels[0]: intensity(im,features)})
-    for i,layer in enumerate(layers):
-        df[labels[i+1]] = pd.Series(intensity(layer,features))
+    # logging.debug("Found %d events." % np.max(features))
+
+    df = pd.DataFrame()
+    for i,layer in enumerate((im,) + layers):
+        df[labels[i]] = pd.Series(intensity(layer,features))
+
     return df
--- scripts/examples/basic_extraction.py
+++ scripts/examples/basic_extraction.py
@@ -3,9 +3,9 @@ import skimage.io as skio
 
 path = '/mnt/seaside/rsl/DataSetForTim/2D_Data/Kiwimagi12082017/CytationData/170811_112033_Plate 3/'
 
-g = skio.imread(path + 'B6_02_2_6_GFP_001.tif')
-r = skio.imread(path + 'B6_02_3_6_Texas Red_001.tif')
-b = skio.imread(path + 'B6_02_4_6_TagBFP_001.tif')
+g = skio.imread(path + 'B5_02_2_6_GFP_001.tif')
+r = skio.imread(path + 'B5_02_3_6_Texas Red_001.tif')
+b = skio.imread(path + 'B5_02_4_6_TagBFP_001.tif')
 
 # segment in green channel, compute intensities in red and blue with green mask
 df = mtcell.get_events(g,(r,b),['g','r','b'])
