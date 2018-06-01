a5b13751a0dad632f7f9489edff186b2e81b0040
Tim Jones <tjones01@gmail.com>
2018-06-01 11:45:16 -0400

    Update readme.

--- scripts/examples/basic_extraction.py
+++ scripts/examples/basic_extraction.py
@@ -7,5 +7,5 @@ g = skio.imread(path + 'B5_02_2_6_GFP_001.tif')
 r = skio.imread(path + 'B5_02_3_6_Texas Red_001.tif')
 b = skio.imread(path + 'B5_02_4_6_TagBFP_001.tif')
 
-# segment in green channel, compute intensities in red and blue with green mask
+# segment in green channel, compute intensities in green, red, and blue with green mask
 df,_,_ = mseg.get_events(g,(g,r,b),['g','r','b'])
